ec13c1563cce8d1396a658ca640944bf7700494c
Ilya Kavalerov <ilyakavalerov@gmail.com>
2018-09-17 16:41:54 -0400

    Reproduce Tang accuracy with too many parameters

--- tang_feat.py
+++ tang_feat.py
@@ -1,10 +1,16 @@
+"""Just feature extraction
+"""
 
 from collections import namedtuple
 import itertools
+import time
 import os
 
+import h5py
+import hdf5storage
 import numpy as np
 import tensorflow as tf
+from tqdm import tqdm
 import scipy.io as sio
 
 import windows as win
@@ -49,23 +55,21 @@ def scat3d(x, win_params, layer_params):
 
     return tf.abs(tf.complex(real1, imag1))
 
-def tang_net(x_dict, reuse):
+def tang_net(x, reuse=False):
     """
     Args:
-        x_dict: with images in (heigh, width, bands) format
+        x: image in (heigh, width, bands) format
     Output:
         center pixel feature vector
     """
     kernel_size = [7,7,7]
-    max_scale = 2
-    K = 1
+    max_scale = 3
+    K = 3
 
     psiO = win.tang_psi_factory(max_scale, K, kernel_size)
     phiO = win.tang_phi_window_3D(max_scale, kernel_size)
 
     with tf.variable_scope('TangNet', reuse=reuse):
-        x = x_dict['images']
-
         x = tf.transpose(x, [2, 0, 1])
 
         x = tf.expand_dims(x, 0)
@@ -98,32 +102,56 @@ def tang_net(x_dict, reuse):
 
     return tf.concat([S0,S1,S2],0)
 
-def tang_run(data):
-    x = np.pad(data, ((9,9),(9,9),(9,9)), 'wrap')
-
-
-
-
-    pixels = np.array(list(itertools.product(range(data.shape[1]),range(data.shape[0]))))
-    for pixel in pixels:
-        # this iterates through columns first
-        [pixel_y, pixel_x] = pixel
-        subimg = x[pixel_x:(pixel_x+19), pixel_y:(pixel_y+19), :]
+def tang_run_IP(data):
+    x = tf.placeholder(tf.float32, shape=(19,19,218))
+    feat = tang_net(x)
     
-        # feat = model_fn(tf.Variable(subimg), psiO, phiO)
-
-    # running
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
-    myres = sess.run(feat)
+
+    padded_data = np.pad(data, ((9,9),(9,9),(9,9)), 'wrap')
+
+    matfiledata = {}
+    matfiledata[u'feats'] = np.zeros((145*145, 151400))
+
+    pixels = np.array(list(itertools.product(range(data.shape[1]),range(data.shape[0]))))
+    for pixel_i, pixel in enumerate(tqdm(pixels)):
+        # this iterates through columns first
+        [pixel_y, pixel_x] = pixel
+        subimg = padded_data[pixel_x:(pixel_x+19), pixel_y:(pixel_y+19), :]
+    
+        feed_dict = {x: subimg}
+        matfiledata[u'feats'][pixel_i,:] = sess.run(feat, feed_dict)
+
+
+    start = time.time()
+    hdf5storage.write(matfiledata, filename='/scratch0/ilya/locDoc/data/hyperspec/features/tang_IP.mat', matlab_compatible=True)
+    end = time.time()
+    print(end - start)
+    pdb.set_trace()
+
+    mat_contents = sio.loadmat('/scratch0/ilya/locDoc/data/hyperspec/Indian_pines_gt_traintest_1_1abefb.mat' )
+    trainX = matfiledata[u'feats'][mat_contents['train_mask'][:,0]==1,:]
+    mat_contentsY = sio.loadmat('/scratch0/ilya/locDoc/data/hyperspec/Indian_pines_gt.mat')
+    all_labels = mat_contentsY['indian_pines_gt'].transpose().reshape(145*145)
+    trainY = all_labels[mat_contents['train_mask'][:,0]==1]
+
+    prob  = svm_problem(trainY.tolist(), trainX.tolist())
+    param = svm_parameter('-s 0 -t 0 -q')
+    m = svm_train(prob, param)
+
+    testX = matfiledata[u'feats'][mat_contents['test_mask'][:,0]==1,:]
+    testY = all_labels[mat_contents['test_mask'][:,0]==1]
+
+    svm_predict(testY.tolist(), testX.tolist(), m)
+
 
 
 if __name__ == '__main__':
-    pdb.set_trace()
     
     data_path = '/scratch0/ilya/locDoc/data/hyperspec'
     data_file = os.path.join(data_path, 'Indian_pines_corrected.mat')
     mat_contents = sio.loadmat(data_file)
     data = mat_contents['indian_pines_corrected'].astype(np.float32)
     data /= np.max(np.abs(data))
-    tang_feat(data)
+    tang_run_IP(data)
