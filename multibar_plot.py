c2837942c340e8334c3101afe1f129783e3a8d60
Ilya Kavalerov <ilya@artsymail.com>
2018-10-29 23:44:57 -0400

    Add avg bars

--- multibar_plot.py
+++ multibar_plot.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import pdb
+
 # median
 noisy2 = np.array([4.46, -1.09, 5.41, -6.06, -7.68])
 class2 = np.array([4.96, 1.84, 0.52, -0.0, -0.0])
@@ -13,7 +15,7 @@ class5 = np.array([-10.23, -12.79, 0.12, 0.28, -2.01])
 
 
 
-N = 5
+N = 6
 ind = np.arange(N)  # the x locations for the groups
 width = 0.15       # the width of the bars
 
@@ -27,25 +29,25 @@ myyellow = (239/mylim,200/mylim,88/mylim)
 myred = (176/mylim,69/mylim,65/mylim)
 myblue = (121/mylim, 160/mylim, 240/mylim)
 
-rects1 = ax.bar(ind, class2-noisy2, width, color=myblue)
-plt.axhline(y=(class2-noisy2).mean(), color=myblue, linestyle='-')
+barhs1 = np.append(class2-noisy2, np.array([(class2-noisy2).mean()]))
+rects1 = ax.bar(ind, barhs1, width, color=myblue)
 
-rects2 = ax.bar(ind+width, pil2-noisy2, width, color=myred)
-plt.axhline(y=(pil2-noisy2).mean(), color=myred, linestyle='-')
+barhs2 = np.append(pil2-noisy2, np.array([(pil2-noisy2).mean()]))
+rects2 = ax.bar(ind+width, barhs2, width, color=myred)
 
-rects3 = ax.bar(ind+width*2, class3-noisy3, width, color=myyellow)
-plt.axhline(y=(class3-noisy3).mean(), color=myyellow, linestyle='-')
+barhs3 = np.append(class3-noisy3, np.array([(class3-noisy3).mean()]))
+rects3 = ax.bar(ind+width*2, barhs3, width, color=myyellow)
 
-rects4 = ax.bar(ind+width*3, pil3-noisy3, width, color=mygreen)
-plt.axhline(y=(pil3-noisy3).mean(), color=mygreen, linestyle='-')
+barhs4 = np.append(pil3-noisy3, np.array([(pil3-noisy3).mean()]))
+rects4 = ax.bar(ind+width*3, barhs4, width, color=mygreen)
 
-rects5 = ax.bar(ind+width*4, class5-noisy5, width, color=mygray)
-plt.axhline(y=(class5-noisy5).mean(), color=mygray, linestyle='-')
+barhs5 = np.append(class5-noisy5, np.array([(class5-noisy5).mean()]))
+rects5 = ax.bar(ind+width*4, barhs5, width, color=mygray)
 
 ax.set_title('Median Scale Invariant SDR Improvement')
 ax.set_ylabel('dB')
 ax.set_xticks(ind+width)
-ax.set_xticklabels( ('Acc', 'Pass', 'Idle', 'Horn', 'Steps') )
+ax.set_xticklabels( ('Acc', 'Pass', 'Idle', 'Horn', 'Steps', 'Avg') )
 ax.legend( (rects1[0], rects2[0], rects3[0], rects4[0], rects5[0]),
 	('Class-2', 'PIL-2', 'Class-3', 'PIL-3', 'Class-5') )
 
@@ -62,4 +64,4 @@ def autolabel(rects):
 # autolabel(rects5)
 ax.grid()
 plt.savefig('/cfarhomes/ilyak/Desktop/icassp_plot.png')
-#plt.savefig('/Users/artsyinc/Desktop/icassp_plot.png')
+# plt.savefig('/Users/artsyinc/Desktop/icassp_plot.png')
