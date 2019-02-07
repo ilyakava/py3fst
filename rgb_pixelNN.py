de3c9d0723248e1177dce19dabc83509d4a1e1ca
Ilya Kavalerov <ilyakavalerov@gmail.com>
2019-02-07 10:37:14 -0500

    Add code that follows human perception of spectra into colors to make false-color HSI images

--- rgb_pixelNN.py
+++ rgb_pixelNN.py
@@ -339,6 +339,119 @@ def normalize_channels(cube):
     cube /= chan_maxes
     return cube
 
+def hyper_rgb_simple_sim(dataset_name, data_struct_field_name):
+    dataset_wvlenghts = {
+        'PaviaU.mat': np.linspace(430, 860, 103),
+        'Pavia_center_right.mat': np.linspace(430, 860, 102),
+        'Indian_pines_corrected.mat': np.linspace(400, 2500, 200),
+        'Salinas_corrected.mat': np.linspace(400, 2500, 204),
+        'Smith_117chan.mat': np.linspace(445, 2486, 117),
+        'Botswana.mat': np.linspace(400, 2500, 145)
+    }
+
+    cannon40d_wbgr_response = np.array([
+        [370, 0.3,    0.3,  0.3],
+        [385, 1.425,  0,    0.35],
+        [400, 2.55,   0,    0.5],
+        [415, 3.675,  0,    0.55],
+        [430, 4.8,   0,    0.6],
+        [445, 5.,    0,      0.8],
+        [461, 4.28,   0.8 ,     0.5 ],
+        [477, 3.57,    1.6,      0],
+        [493, 2.85,    2.4,      0],
+        [509, 2.14,    3.2,      0],
+        [525, 1.42,    3.2,      0],
+        [541, 0.71,    3.3,      0],
+        [557, 0.0,    3.2,      1.1],
+        [573, 0,    2.5,      2.1],
+        [589, 0,    1.9,      2.7],
+        [605, 0,    1.28,      2.9],
+        [621, 0,    0.64,      2.7],
+        [637, 0.2,    0,      2.60],
+        [653, 0.35,    0,      2.50],
+        [669, 0.4,    0.09,      2.40],
+        [685, 0.3,    0.26,      2.30],
+        [701, 0.2,    0.35,      2.21],
+        [717, 0.15,    0.44,      2.11],
+        [733, 0.1,    0.53,      2.01],
+        [749, 0,    0.62,      1.91],
+        [765, 0,    0.71,      1.81],
+        [781, 0.3,    0.8,      1.72],
+        [797, 1.3,    0.8,      1.62],
+        [813, 1.5,    0.74,      1.52],
+        [829, 1.5,    0.69,      1.42],
+        [845, 1.3,    0.63,      1.33],
+        [861, 1.2,    0.58,      1.23],
+        [877, 1.1,    0.52,      1.13],
+        [893, 1,    0.47,      1.03],
+        [909, 0.8,    0.42,      0.93],
+        [925, 0.6,    0.36,      0.84],
+        [941, 0.5,    0.31,      0.74],
+        [957, 0.4,    0.25,      0.64],
+        [973, 0.3,    0.20,      0.54],
+        [989, 0.25,    0.15,      0.45]])
+
+    # CIE_1931_XYZ_Color_Matching_Functions
+    # https://en.wikipedia.org/wiki/CIE_1931_color_space#Color_matching_functions
+    cie1931cmf_wbgr_response = np.array([
+        [390,   0   ,   0   ,      0],
+        [410,   0.18,   0   ,      0.03],
+        [430,   1.43,   0   ,      0.3],
+        [450,   1.78,   0.05,      0.33],
+        [470,   1.28,   0.9 ,      0.2],
+        [490,   0.5 ,   0.2 ,      0.03],
+        [510,   0.16,   0.5 ,      0],
+        [530,   0.05,   0.87,      0.16],
+        [550,   0   ,   1   ,      0.44],
+        [570,   0   ,   0.96,      0.78],
+        [590,   0   ,   0.77,      1.02],
+        [610,   0   ,   0.5 ,      1.02],
+        [630,   0   ,   0.24,      0.68],
+        [650,   0   ,   0.1 ,      0.3],
+        [670,   0   ,   0   ,      0.1],
+        [690,   0   ,   0   ,      0]])
+
+    spec_response = cie1931cmf_wbgr_response
+
+
+
+    mat_contents = sio.loadmat(os.path.join(DATASET_PATH, dataset_name))
+    data = mat_contents[data_struct_field_name].astype(np.float32)
+
+    response_mask = dataset_wvlenghts[dataset_name] < spec_response[:,0].max()
+    blue_weights = response_mask * np.interp(dataset_wvlenghts[dataset_name], spec_response[:,0], spec_response[:,1])
+    green_weights = response_mask * np.interp(dataset_wvlenghts[dataset_name], spec_response[:,0], spec_response[:,2])
+    red_weights = response_mask * np.interp(dataset_wvlenghts[dataset_name], spec_response[:,0], spec_response[:,3])
+
+    ratios = np.array(spec_response)[5:,:]
+    blue = np.expand_dims(np.sum(blue_weights*data,axis=2),-1)
+    green = np.expand_dims(np.sum(green_weights *data,axis=2),-1)
+    red = np.expand_dims(np.sum(red_weights *data,axis=2),-1)
+
+    color = normalize_channels(np.concatenate([red, green,blue], axis=2))
+
+    # plt.imshow(color)
+    # plt.show()
+
+    plt.imsave('/scratch0/ilya/locDownloads/%s_color.png' % data_struct_field_name, color)
+
+def all_hyper_rgb_simple_sim():
+
+    hyper_rgb_simple_sim('Smith_117chan.mat', 'Smith')
+
+    hyper_rgb_simple_sim('Indian_pines_corrected.mat', 'indian_pines_corrected')
+
+    hyper_rgb_simple_sim('Salinas_corrected.mat', 'salinas_corrected')
+
+    # hyper_rgb_simple_sim('KSC_corrected.mat', 'KSC')
+
+    hyper_rgb_simple_sim('Botswana.mat', 'Botswana')
+
+    hyper_rgb_simple_sim('PaviaU.mat', 'paviaU')
+
+    hyper_rgb_simple_sim('Pavia_center_right.mat', 'Pavia_center_right')
+
+
 def hyper_rgb_sim():
     cannon40d_wbgr_response = [
         [370, 0.3,    0.3,  0.3],
@@ -631,7 +744,7 @@ def kaggle_test(outpath='/scratch0/ilya/locDoc/data/kaggle-seismic-dataset/predi
 
 
 if __name__ == '__main__':
-    scroll_thru_hyper()
+    all_hyper_rgb_simple_sim()
 
     # lets look at the result images with the scroll thru vis
     # then do the mnist like network on binary and see results (with PCA layer in between)
