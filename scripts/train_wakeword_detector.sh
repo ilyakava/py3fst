e7133f5afb40fc4859032470ba1701c1a92db522
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-03-25 11:39:59 -0400

    Minor: Renames wakeword detector script

--- scripts/train_wakeword_detector.sh
+++ scripts/train_wakeword_detector.sh
@@ -12,7 +12,7 @@
 
 CUDA_VISIBLE_DEVICES=0 python spectrogram_NN.py \
 --model_root=/scratch0/ilya/locDoc/adv_audio_tf_board/spectrogram03.20.20 \
---train_data_root /scratch1/ilya/locDoc/data/alexa/v4e/train/ \
---val_data_root /scratch1/ilya/locDoc/data/alexa/v4e/val/ \
+--train_data_root /scratch1/ilya/locDoc/data/alexa/v4e/train \
+--val_data_root /scratch1/ilya/locDoc/data/alexa/v4e/val \
 --feature_height 256 \
 --hop_length 160
