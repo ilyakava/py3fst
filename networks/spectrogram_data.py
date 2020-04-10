7402af2da8628fcfcb074fc5ba5b30f0d002d73f
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-04-10 10:46:59 -0400

    Adds parser for centered wakeword in example

--- networks/spectrogram_data.py
+++ networks/spectrogram_data.py
@@ -34,7 +34,53 @@ def time_cut_parser(serialized_example, h, in_w, out_w):
     label = tf.cast(features['spectrogram_label'], tf.int32)
     spec = tf.reshape(spec, (h, in_w))
     
-    si = random.randint(0, in_w - out_w - 1)
+    if in_w == out_w:
+        si = 0
+    else:
+        si = random.randint(0, in_w - out_w - 1)
+    ei = si + out_w
+    
+    return spec[:,si:ei], label[si:ei]
+
+def time_cut_centered_wakeword_parser(serialized_example, h, in_w, out_w):
+    """Parses a single tf.Example into x,y tensors.
+    
+    Does data augmentation by selecting a random
+    sub-sequence at parse time.
+    
+    Assumes that there is a wakeword starting at in_w//2.
+    Guaranteed to be no wakeword if you start 0 ... in_w//2 - out_w, in_w//2+wakeword_len_max ... in_w - out_w
+    Guaranteed to be a whole wakeword if you start w//2 + wakeword_len_max - out_w ... w//2
+    Otherwise may partially overlap.
+    
+    """
+    features = tf.parse_single_example(
+      serialized_example,
+      features={
+          'spectrogram': tf.FixedLenFeature([h * in_w], tf.float32),
+          'spectrogram_label': tf.FixedLenFeature([in_w], tf.int64),
+      })
+    spec = features['spectrogram']
+    label = tf.cast(features['spectrogram_label'], tf.int32)
+    spec = tf.reshape(spec, (h, in_w))
+    
+    wakeword_len_min=39
+    wakeword_len_max=89
+    case_rv = random.random()
+    
+    if case_rv < 0.33:
+        only_n_starts = set(range(0, in_w//2 - out_w)) | set(range(in_w//2+wakeword_len_max, in_w - out_w))
+        si = random.sample(only_n_starts,1)[0]
+    elif case_rv < 0.66:
+        only_p_starts = set(range(in_w//2 + wakeword_len_max - out_w, in_w//2))
+        si = random.sample(only_p_starts,1)[0]
+    else:
+        only_n_starts = set(range(0, in_w//2 - out_w)) | set(range(in_w//2+wakeword_len_max, in_w - out_w))
+        only_p_starts = set(range(in_w//2 + wakeword_len_max - out_w, in_w//2))
+        all_starts = set(range(0,in_w - out_w))
+        either_n_or_p = (all_starts - only_n_starts) - only_p_starts
+        si = random.sample(either_n_or_p,1)[0]
+    
     ei = si + out_w
     
     return spec[:,si:ei], label[si:ei]
