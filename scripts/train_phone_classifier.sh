376cc3314d0dd90ee5b6a06cff34cc8ba228bfcf
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-07-15 21:43:57 -0400

    Updates scripts with shifting and centering.

--- scripts/train_phone_classifier.sh
+++ scripts/train_phone_classifier.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-# This is an example script for how to train a phone classifier
-
+# This is an example script for how to train a phone classifier.
 
 CUDA_VISIBLE_DEVICES=0 python spectrogram_phoneme_classifier.py \
---model_root=/vulcan/scratch/ilyak/experiments/phones_tf_board/CBHG_skinny_v0.3_gwn_15dBFS_48000 \
---train_data_root /vulcan/scratch/ilyak/data/phones/v0.3_spec_hop10_gwn_15dBFS/train_47840 \
---val_data_root /vulcan/scratch/ilyak/data/phones/v0.3_spec_hop10_gwn_15dBFS/val_47840 \
---val_shift 0.058074123157292545 \
---val_center 0.242303276164229 \
---train_shift 0.05836660828353991 \
---train_center 0.2406181628816118 \
+--model_root=/vulcan/scratch/ilyak/experiments/phones_tf_board/CBHG_skinny_v0.4_gwn_15dBFS_48000 \
+--train_data_root /vulcan/scratch/ilyak/data/phones/v0.4_spec_hop10_gwn_15dBFS/train_47840 \
+--val_data_root /vulcan/scratch/ilyak/data/phones/v0.4_spec_hop10_gwn_15dBFS/val_47840 \
+--val_shift 0.28499622849111 \
+--val_center 0.18031198767476836 \
+--train_shift 0.28605913762866186 \
+--train_center 0.18103149871955165 \
 --feature_height 257 \
 --tfrecord_feature_width 300 \
 --network_feature_width 300 \
--- scripts/train_wakeword_detector.sh
+++ scripts/train_wakeword_detector.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
+# This is an example script for how to train a wakeword detector.
 
-# python spectrogram_NN.py \
-# --model_root=/scratch0/ilya/locDoc/adv_audio_tf_board/cortical_drop0.2_bs64_05.22_v7 \
-# --train_data_root /scratch1/ilya/locDoc/data/alexa/v7/train_80000 \
-# --val_data_root /scratch1/ilya/locDoc/data/alexa/v7/val_19840 \
-# --feature_height 256 \
-# --lr 0.00005 \
-# --tfrecord_train_feature_width 500 \
-# --tfrecord_eval_feature_width 124 \
-# --batch_size 64 \
-# --dropout 0.2
-
-python spectrogram_NN.py \
---model_root=/scratch0/ilya/locDoc/adv_audio_tf_board/cortical_drop0.6_bs64_05.22_v7 \
---train_data_root /scratch1/ilya/locDoc/data/alexa/v7/train_80000 \
---val_data_root /scratch1/ilya/locDoc/data/alexa/v7/val_19840 \
---feature_height 256 \
+CUDA_VISIBLE_DEVICES=0 python spectrogram_NN.py \
+--model_root=/vulcan/scratch/ilyak/experiments/wake_tf_board/baseline_bs64_v7.4_warm_trial2 \
+--train_data_root /vulcan/scratch/ilyak/data/alexa/v7.4/train_79840 \
+--val_data_root /vulcan/scratch/ilyak/data/alexa/v7.4/val_19680 \
+--val_shift 0.3343832678897679 \
+--val_center 0.20798715062082082 \
+--train_shift 0.3246136855279846 \
+--train_center 0.19699797545310258 \
+--feature_height 257 \
 --lr 0.00005 \
 --tfrecord_train_feature_width 500 \
 --tfrecord_eval_feature_width 124 \
 --batch_size 64 \
---dropout 0.6
+--dropout 0.2 \
+--network_name Guo_Li_net \
+--pretrain_checkpoint=/vulcan/scratch/ilyak/experiments/phones_tf_board/CBHG_skinny_v0.4_gwn_15dBFS_48000/export/best_loss_exporter/1594436033/variables/variables
