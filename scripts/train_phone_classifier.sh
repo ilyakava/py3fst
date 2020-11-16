08bb90c06d08ef09d881ef8e2e44fdccac5bb368
Ilya Kavalerov <ilyakavalerov@gmail.com>
2020-11-16 11:53:50 -0500

    Minor: vulcan scratch location change (these scripts are outdated however)

--- scripts/train_phone_classifier.sh
+++ scripts/train_phone_classifier.sh
@@ -2,9 +2,9 @@
 # This is an example script for how to train a phone classifier.
 
 CUDA_VISIBLE_DEVICES=0 python spectrogram_phoneme_classifier.py \
---model_root=/vulcan/scratch/ilyak/experiments/phones_tf_board/CBHG_skinny_v0.4_gwn_15dBFS_48000 \
---train_data_root /vulcan/scratch/ilyak/data/phones/v0.4_spec_hop10_gwn_15dBFS/train_47840 \
---val_data_root /vulcan/scratch/ilyak/data/phones/v0.4_spec_hop10_gwn_15dBFS/val_47840 \
+--model_root=/vulcanscratch/ilyak/experiments/phones_tf_board/CBHG_skinny_v0.4_gwn_15dBFS_48000 \
+--train_data_root /vulcanscratch/ilyak/data/phones/v0.4_spec_hop10_gwn_15dBFS/train_47840 \
+--val_data_root /vulcanscratch/ilyak/data/phones/v0.4_spec_hop10_gwn_15dBFS/val_47840 \
 --val_shift 0.28499622849111 \
 --val_center 0.18031198767476836 \
 --train_shift 0.28605913762866186 \
--- scripts/train_wakeword_detector.sh
+++ scripts/train_wakeword_detector.sh
@@ -2,9 +2,9 @@
 # This is an example script for how to train a wakeword detector.
 
 CUDA_VISIBLE_DEVICES=0 python spectrogram_NN.py \
---model_root=/vulcan/scratch/ilyak/experiments/wake_tf_board/baseline_bs64_v7.4_warm_trial2 \
---train_data_root /vulcan/scratch/ilyak/data/alexa/v7.4/train_79840 \
---val_data_root /vulcan/scratch/ilyak/data/alexa/v7.4/val_19680 \
+--model_root=/vulcanscratch/ilyak/experiments/wake_tf_board/baseline_bs64_v7.4_warm_trial2 \
+--train_data_root /vulcanscratch/ilyak/data/alexa/v7.4/train_79840 \
+--val_data_root /vulcanscratch/ilyak/data/alexa/v7.4/val_19680 \
 --val_shift 0.3343832678897679 \
 --val_center 0.20798715062082082 \
 --train_shift 0.3246136855279846 \
@@ -16,4 +16,4 @@ CUDA_VISIBLE_DEVICES=0 python spectrogram_NN.py \
 --batch_size 64 \
 --dropout 0.2 \
 --network_name Guo_Li_net \
---pretrain_checkpoint=/vulcan/scratch/ilyak/experiments/phones_tf_board/CBHG_skinny_v0.4_gwn_15dBFS_48000/export/best_loss_exporter/1594436033/variables/variables
+--pretrain_checkpoint=/vulcanscratch/ilyak/experiments/phones_tf_board/CBHG_skinny_v0.4_gwn_15dBFS_48000/export/best_loss_exporter/1594436033/variables/variables
